a0602961d883525ce3a023638408b03134b21ad1
DurgaSwaroop <durgaswaroop@gmail.com>
2017-04-05 12:33:20 +0530

    Add Javadoc

--- src/main/java/com/freblogg/hashing/FNV.java
+++ src/main/java/com/freblogg/hashing/FNV.java
@@ -1,7 +1,15 @@
 package com.freblogg.hashing;
 
 /**
- * Created by swaroop on 04-04-2017.
+ * Implementation of the Fowler-Noll-Vo non-cryptographic fast hashing algorithms originally created by Glenn Fowler,
+ * Landon Curt Noll and Kiem-Phong Vo.
+ * Consists of implementation of FNV132, FNV1a32, FNV164 and FNV1a64 algorithms.
+ * These algorithms are fast with low collision rates.
+ * <p>
+ * Corresponding methods can be called statically.
+ * For example, for FNV1a64 hash of a string, Use FNV.fnv1a64(..).
+ *
+ * @author : Swaroop [durgaswaroop@gmail.com]
  */
 public class FNV {
 
@@ -14,34 +22,85 @@ public class FNV {
     private static final long FNV64_PRIME = 1_099_511_628_211L;
     private static final long FNV64_MOD_POWER = (long) Math.pow(2, 64);
 
+    private FNV() {
+    }
+
+    /**
+     * Calculates FNV132 hash of the input string.
+     *
+     * @param input String on which hash needs to be calculated
+     * @return 8-character hash for the input string
+     */
     public static String fnv132(String input) {
         return fnv132(getBytes(input));
     }
 
+    /**
+     * Calculates FNV132 hash for the input bytes.
+     *
+     * @param bytes bytes for which hash needs to be calculated
+     * @return 8-character hash for the input bytes
+     */
     public static String fnv132(byte[] bytes) {
         return fnv1(bytes, FNV32_OFFSET_BASIS, FNV32_PRIME, FNV32_MOD_POWER, 8);
     }
 
+    /**
+     * Calculates FNVa132 hash for the input string.
+     *
+     * @param input String on which hash needs to be calculated
+     * @return 8-character hash for the input string
+     */
     public static String fnv1a32(String input) {
         return fnv1a32(getBytes(input));
     }
 
+    /**
+     * Calculates FNVa132 hash for the input bytes.
+     *
+     * @param bytes bytes for which hash needs to be calculated
+     * @return 8-character hash for the input bytes
+     */
     public static String fnv1a32(byte[] bytes) {
         return fnv1a(bytes, FNV32_OFFSET_BASIS, FNV32_PRIME, FNV32_MOD_POWER, 8);
     }
 
+    /**
+     * Calculates FNV164 hash for the input string.
+     *
+     * @param input String on which hash needs to be calculated
+     * @return 16-character hash for the input string
+     */
     public static String fnv164(String input) {
         return fnv164(getBytes(input));
     }
 
+    /**
+     * Calculates FNV164 hash for the input bytes.
+     *
+     * @param bytes bytes for which hash needs to be calculated
+     * @return 16-character hash for the input bytes
+     */
     public static String fnv164(byte[] bytes) {
         return fnv1(bytes, FNV64_OFFSET_BASIS, FNV64_PRIME, FNV64_MOD_POWER, 16);
     }
 
+    /**
+     * Calculates FNVa164 hash for the input string.
+     *
+     * @param input String on which hash needs to be calculated
+     * @return 16-character hash for the input string
+     */
     public static String fnv1a64(String input) {
         return fnv1a64(getBytes(input));
     }
 
+    /**
+     * Calculates FNVa164 hash for the input bytes.
+     *
+     * @param bytes bytes for which hash needs to be calculated
+     * @return 16-character hash for the input bytes
+     */
     public static String fnv1a64(byte[] bytes) {
         return fnv1a(bytes, FNV64_OFFSET_BASIS, FNV64_PRIME, FNV64_MOD_POWER, 16);
     }
